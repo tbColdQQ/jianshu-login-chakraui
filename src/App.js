c98c589935c33ed8c83dad0c7c62c2708fa867e0
tbColdQQ <947678182@qq.com>
2021-01-29 23:21:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Image, Box, useColorModeValue, Flex } from '@chakra-ui/react'
+import LogoLightImg from './assets/images/logo-light.png'
+import LogoDarkImg from './assets/images/logo-light.png'
 
 function App() {
+
+  const bgColor = useColorModeValue('#F1F1F1', 'black')
+  const logoImg = useColorModeValue(LogoLightImg, LogoDarkImg)
+  const imgProps = {
+    pos: 'absolute',
+    top: '56px',
+    left: '50px'
+  }
+  const contentBox = {
+    w: '820px',
+    h: '600px',
+    pos: 'relative',
+    m: '0 auto',
+    bgGradient: 'url(//cdn2.jianshu.io/assets/web/sign_in_bg-bbc515a9c2e7734807ea281b9b2ab380.png)',
+    bgRepeat: 'no-repeat',
+    bgSize: '40%',
+    bgPos: '45px 0'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box w="100%" h="100%" bgColor={bgColor}>
+      <Image src={logoImg} w='100px' {...imgProps}/>
+      <Flex flexDirection="column" align="center" h="100%">
+        <Box {...contentBox}></Box>
+      </Flex>
+    </Box>
   );
 }
 
